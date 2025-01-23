b70c0d720701a8dbf80e95da07df791728804cf7
pratiksha1598 <pratikshalagade1998@gmail.com>
2025-01-23 17:23:38 +0530

    Delete Calculator directory

--- Calculator/src/components/Display.jsx
+++
@@ -1,9 +0,0 @@
-import css from '../App.module.css';
-
-let Display = ({displayValue}) => {
-  return (
-    <input className={css.display} type="text" value={displayValue} readOnly/>
-  );
-};
-
-export default Display;
